1fe1caeeaa37b0a462f0a28421cf2bfb60959c20
ggrl <gabrielgoes@usp.br>
2024-03-18 09:09:03 -0300

    tentando debugar

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -3,6 +3,7 @@ require'nvim-treesitter.configs'.setup {
 --    ensure_installed = {'python', 'markdown', 'latex'},
     sync_install = true,
     auto_install = true,
+
     highlight = {
         -- 'false' will disable the whole extension
       enable = true,
