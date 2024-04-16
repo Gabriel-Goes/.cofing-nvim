1265c02e7b4469d637710fe7445bc746c4c52ac6
ggrl <gabrielgoes@usp.br>
2024-04-16 07:54:38 -0300

    config de pylsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -105,6 +105,36 @@ require('lspconfig').lua_ls.setup{
 require'lspconfig'.pylsp.setup{
     cmd = {vim.fn.stdpath('data') .. '/mason/bin/pylsp'},
     on_attach = on_attach,
+    settings = {
+        pylsp = {
+            plugins = {
+                pycodestyle = {
+                    enabled = true,
+                    ignore = {
+                        'E501'
+                    },
+                },
+                jedi_completion = {
+                    enabled = true,
+                },
+                jedi_definition = {
+                    enabled = true,
+                },
+                jedi_hover = {
+                    enabled = true,
+                },
+                jedi_references = {
+                    enabled = true,
+                },
+                jedi_signature_help = {
+                    enabled = true,
+                },
+                jedi_symbols = {
+                    enabled = true,
+                },
+            },
+        },
+    },
 }
 -------------- LaTex
 require'lspconfig'.ltex.setup{
